21c9de66a12d5baf2d87f36c1c29934781405d4c
min-7343 <mky3eong@gmail.com>
2025-02-12 15:03:35 +0900

    250212 수 5교시

--- java_se/src/day0212/UseList.java
+++ java_se/src/day0212/UseList.java
@@ -1,8 +1,12 @@
 package day0212;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Vector;
+
+import javax.annotation.processing.SupportedSourceVersion;
 
 /**
  * List 사용<br>
@@ -55,6 +59,12 @@ public class UseList {
 		list2.add(2025);// 원래는 안들어가는 것이 정상 => autoboxing 기능 제공.
 		//list2.add(new Integer(2025));  // 생성자는 JDK1.8가지 사용 가능 //Integer -> 참조형  2025 -> 기본형
 		list2.add(Integer.valueOf(2025)); //Java SE 9부터는 valueOf 사용
+		list2.add(2);
+		list2.add(2026);
+		list2.add(3000);
+		
+		
+		
 		
 		Integer[] intArr=new Integer[list2.size()];
 		list2.toArray(intArr); //배열로 복사
@@ -63,16 +73,58 @@ public class UseList {
 //			System.out.println();
 //		}
 		
-		
 		//값얻기
-		
 		String temp=list.get(0);
 		Integer i=list2.get(0);
-		int iv=list2.get(0); //
+		int iv=list2.get(0); //unboxing : wrapper가 반환되면 Wrapper class의 method를
+		//호출하여 ( intValue()) 기본형 데이터에 할당을 해준다. 레퍼클래스가 갖고 있는 값을 꺼낼때 알아서 자신의 기본형으로 반응하는 것.
+		
+		temp="";
+		for(int j=0;j<list.size();j++) {
+			temp=list.get(j);//리스트 인덱스에 해당하는 값을 얻어서
+			System.out.println(temp);
+		}//end for
+		
+//		for(Integer j : list2 ) {//unboxing
+//			System.out.println(j); //j.toString
+//		}//end for
+//		
+//		System.out.println(list);
+		System.out.println("---------------------------------");
+		//삭제
+		//인덱스
+//		list.remove(1); //1번 : 오늘은 => 사라짐, 방의 개수도 적어진다.
+		//방의 내용과 일치하는 방 삭제
+//		list.remove("오늘은");
+		
+//		방의 내용과 일치하는 방 삭제 : 방에 동일한 값이 있다면 L->R로 진행하면 가장 처음 일치하는 방이 삭제
+		list.remove("수요일"); 
+		
+		//모은 방의 값 삭제.
+		list.clear();
+		System.out.println(list + " / 방의 개수 : "+list.size());
+		
+		//Arrayw 배열 관련된 업무를 처리하는 일.
+		System.out.println("배열의 방의 값 출력");
+		System.out.println( Arrays.toString(strArr) ); //주소가 출력
+		System.out.println( Arrays.toString(intArr) ); //주소가 출력
+		//배열을 정렬
+		
+		Arrays.sort(intArr); //오름차순 정렬
+		System.out.println(Arrays.toString(intArr));
+		Arrays.sort(intArr,Collections.reverseOrder()); //내림차순 정렬
+		System.out.println(Arrays.toString(intArr));
+		
+		System.out.println("리스트가 비었는지?");
+		System.out.println("<String> : "+list.isEmpty());  //
+		System.out.println("<Integer> : "+list2.isEmpty()); //generic 사용 list
 		
 		
-		System.out.println(list + " / 방의 개수 : " + list.size());
-		System.out.println(list2 + " / 방의 개수 : " + list2.size());
+//		System.out.println(temp);
+//		System.out.println(i.toString());//i = i.toString => 문자열로 나온 것이다. 
+//		System.out.println(iv);
+//		System.out.println(list + " / 방의 개수 : " + list.size());
+//		System.out.println(list2 + " / 방의 개수 : " + list2.size());
 	
 	}// useArrayList
 
