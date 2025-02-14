3f2c2f6fd8308c7d55ae472ecfe6c573d64412b7
USER <USER@DESKTOP-S0CN6A6>
2025-02-15 01:56:17 +0900

    240214 숙제

--- java_se/src/day0214/CalendarButton.java
+++ java_se/src/day0214/CalendarButton.java
@@ -1,36 +1,39 @@
 package day0214;
 
-
+import java.awt.GridLayout;
 import java.util.Calendar;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 //1.윈도우에서 상속받기
 @SuppressWarnings("serial")
-public class CalendarButton extends JFrame{
+public class CalendarButton extends JFrame {
 
 	public CalendarButton() {
 		super("달력입니다.");
-		//2.컴포넌트를 생성
-		CalendarButton calendar = new CalendarButton();
-		String[] day=calendar.Calendar("2025","2").toString().split(",");
+		// 2.컴포넌트를 생성
+		String[] day =Calendar("2025", "2").toString().split(",");
+		JButton[] calJbt=new JButton[day.length];
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//3.배치관리자를 설정하고 , 컴포넌트 배치
-		add(dbt, "Cental");
+		JPanel jpCenter=new JPanel();
+		jpCenter.setLayout(new GridLayout(6,7));
+		//
+		for(int i=0;i<day.length;i++) {
+			calJbt[i]=new JButton(day[i]);
+			
+			jpCenter.add(calJbt[i]);
+		}
+
+//		for(int i=0;i<day.length;i++) {
+//			if(i%7==0) {System.out.println();}
+//			System.out.printf(day[i]+"\t");
+//		}
+
+//		//3.배치관리자를 설정하고 , 컴포넌트 배치
+		add("Center",jpCenter);
 		//4.윈도우 크기 설정
 		setSize(560,600);
 		//5.사용자에게 보여주고
@@ -40,23 +43,31 @@ public class CalendarButton extends JFrame{
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
 	}
-	
-	
-	private void Calendar(String year, String month) {
-		Calendar cal= Calendar.getInstance();
-		int yearInt=Integer.parseInt(year);
-		int monthInt=Integer.parseInt(month);
-		cal.set(Calendar.YEAR, yearInt);
-		cal.set(Calendar.MONTH, monthInt);
-		
-		int endDay=cal.getMaximum(Calendar.DAY_OF_MONTH);
-		int starDay=cal.get
-	}
 
+	public String Calendar(String year, String month) {
+		Calendar cal = Calendar.getInstance();
+		int yearInt = Integer.parseInt(year);
+		int monthInt = Integer.parseInt(month);
+		cal.set(Calendar.YEAR, yearInt);
+		cal.set(Calendar.MONTH, monthInt-1);
+		StringBuilder sb = new StringBuilder();
+		int date = 1;
+		int endDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int startDay = cal.get(Calendar.DAY_OF_WEEK);
+
+		for (int i = 1; i < 43; i++) {
+			if (i < startDay) {
+				sb.append(" ").append(",");
+			}else if( i>= startDay && i <endDay+startDay) {
+				sb.append(date++).append(",");
+			}else {
+				sb.append(" ").append(",");
+			}
+		}
+		return sb.toString();
+	}
 
 	public static void main(String[] args) {
 		new CalendarButton();
-
 	}
-
 }
